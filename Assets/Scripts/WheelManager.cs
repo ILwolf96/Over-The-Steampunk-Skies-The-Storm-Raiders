0d7b4b6af42b4e375e1a2a2fbf47508309ee6fd9
SaharGGJ <124356732+SaharGGJ@users.noreply.github.com>
2023-06-19 19:34:41 +0300

    Wheel Update
    It moves!

---
+++ Assets/Scripts/WheelManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelManager : MonoBehaviour
+{
+    [SerializeField] private GameObject wheel;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.touchCount>0)
+        {
+            if(Input.GetTouch(0).phase== TouchPhase.Began)
+            {
+                if (Input.GetTouch(0).position.x < Screen.width / 2)
+                {
+                    Debug.Log("Left!");
+                    wheel.transform.Rotate(new Vector3(wheel.transform.rotation.x, wheel.transform.rotation.y, wheel.transform.rotation.z + 15));
+                }
+                else if (Input.GetTouch(0).position.x > Screen.width / 2)
+                {
+                    Debug.Log("Right!");
+                    wheel.transform.Rotate(new Vector3(wheel.transform.rotation.x, wheel.transform.rotation.y, wheel.transform.rotation.z - 15));
+                }
+                else
+                    Debug.Log("Center!");
+            }
+            //if(Input.GetTouch(0).position.x)
+        }
+    }
+}

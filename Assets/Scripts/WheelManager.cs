810dc56a63b09c61e8f0b67ce4782827119331f3
SaharMeir <124356732+SaharGGJ@users.noreply.github.com>
2023-06-20 17:04:04 +0300

    New Wheel
    THE WHEEL NOW TWEENS LEGGO

--- Assets/Scripts/WheelManager.cs
+++ Assets/Scripts/WheelManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,14 @@ using UnityEngine;
 public class WheelManager : MonoBehaviour
 {
     [SerializeField] private GameObject wheel;
-    public float rotateSpeed;
-
+    [SerializeField] private AnimationCurve wheelCurve;
+    public float rotateSpeed = 15f;
+    private Vector3 currentPosition;
+    private Vector3 nextposition;
+    private void Start()
+    {
+        currentPosition = new Vector3(wheel.transform.rotation.x, wheel.transform.rotation.y, wheel.transform.rotation.z);
+    }
     // Update is called once per frame
     private void Update()
     {
@@ -14,20 +21,26 @@ public class WheelManager : MonoBehaviour
         {
             if(Input.GetTouch(0).phase== TouchPhase.Began) // Checks if touch has began.
             {
+                
                 if (Input.GetTouch(0).position.x < Screen.width / 2) // if its on the left side of the screen, rotate the wheel left.
                 {
-                    Debug.Log("Left!");
-                    wheel.transform.Rotate(new Vector3(wheel.transform.rotation.x, wheel.transform.rotation.y, wheel.transform.rotation.z + 15));
+                    nextposition.z=currentPosition.z+15;
+                    currentPosition = nextposition;
+                    Debug.Log("Left! " + nextposition.z);
+                    //wheel.transform.Rotate(new Vector3(wheel.transform.rotation.x, wheel.transform.rotation.y, wheel.transform.rotation.z + 15));
+                    wheel.transform.DORotate(nextposition,rotateSpeed).SetEase(wheelCurve);
                 }
                 else if (Input.GetTouch(0).position.x > Screen.width / 2) // if its on the right side of the screen, rotate the wheel right.
                 {
                     Debug.Log("Right!");
-                    wheel.transform.Rotate(new Vector3(wheel.transform.rotation.x, wheel.transform.rotation.y, wheel.transform.rotation.z - 15));
+                    // wheel.transform.Rotate(new Vector3(wheel.transform.rotation.x, wheel.transform.rotation.y, wheel.transform.rotation.z - 15));
+                    nextposition.z = currentPosition.z - 15;
+                    currentPosition = nextposition;
+                    wheel.transform.DORotate(nextposition, rotateSpeed).SetEase(wheelCurve);
                 }
                 else
                     Debug.Log("Center!");
             }
-            //if(Input.GetTouch(0).position.x)
         }
     }
 }

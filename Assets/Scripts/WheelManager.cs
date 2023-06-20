6f7e6db4bc46af499fa20c1c61e56ce337f4a64a
SaharMeir <124356732+SaharGGJ@users.noreply.github.com>
2023-06-20 16:12:25 +0300

    Comments update

--- Assets/Scripts/WheelManager.cs
+++ Assets/Scripts/WheelManager.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class WheelManager : MonoBehaviour
 {
     [SerializeField] private GameObject wheel;
+    public float rotateSpeed;
 
     // Update is called once per frame
     private void Update()
     {
         if(Input.touchCount>0)
         {
-            if(Input.GetTouch(0).phase== TouchPhase.Began)
+            if(Input.GetTouch(0).phase== TouchPhase.Began) // Checks if touch has began.
             {
-                if (Input.GetTouch(0).position.x < Screen.width / 2)
+                if (Input.GetTouch(0).position.x < Screen.width / 2) // if its on the left side of the screen, rotate the wheel left.
                 {
                     Debug.Log("Left!");
                     wheel.transform.Rotate(new Vector3(wheel.transform.rotation.x, wheel.transform.rotation.y, wheel.transform.rotation.z + 15));
                 }
-                else if (Input.GetTouch(0).position.x > Screen.width / 2)
+                else if (Input.GetTouch(0).position.x > Screen.width / 2) // if its on the right side of the screen, rotate the wheel right.
                 {
                     Debug.Log("Right!");
                     wheel.transform.Rotate(new Vector3(wheel.transform.rotation.x, wheel.transform.rotation.y, wheel.transform.rotation.z - 15));

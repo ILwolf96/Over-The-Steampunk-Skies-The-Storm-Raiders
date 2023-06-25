c7faaa7aad10b2cb354a9896ecbcfa0d39fa3594
SaharMeir <124356732+SaharGGJ@users.noreply.github.com>
2023-06-25 16:54:12 +0300

    Adding Code!

--- Assets/Scripts/EngineManager.cs
+++ Assets/Scripts/EngineManager.cs
@@ -9,6 +9,8 @@ public class EngineManager : MonoBehaviour
     [SerializeField] GameObject fLever;
     [SerializeField] GameObject mbLever;
     [SerializeField] GameObject bLever;
+    private Vector2 originalPosition; //tracks original position.
+    private Vector2 direction; //Tracks direction.
     /*Notes:
      1: Detect via swipes for with phase "moved".
      2: Sort the button using borders the same way the wheel is done.
@@ -28,6 +30,31 @@ public class EngineManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            switch(touch.phase)
+            {
+                case TouchPhase.Began:
+                    //records initial position
+                    originalPosition = touch.position;
+                    break;
+                case TouchPhase.Moved:
+                    direction=touch.position - originalPosition;
+                    break;
+                case TouchPhase.Ended:
+                    switch(direction.y)
+                    {
+                        case 0: Debug.Log("Nothing Happened");
+                            break;
+                        case < 0: Debug.Log("Slowing Down/Going Backwards!");
+                            break;
+                        case > 0: Debug.Log("Speeding Up!");
+                            break;
+                    }
+                    break;
+
+            }
+        }
     }
 }

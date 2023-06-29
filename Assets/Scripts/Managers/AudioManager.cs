e2cacf46e6d9528accc316b513e1ec0ef348e05d
ILwolf96 <ILwolf96@gmail.com>
2023-06-29 23:49:05 +0300

    New Storm Prep

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -7,30 +7,30 @@ public class AudioManager : MonoBehaviour
     public AudioSource startGameplayAudio;
     public AudioSource gameplayLoopAudio;
     public AudioSource endGameplayAudio;
-
     public AudioSource bossFightAudio;
     public AudioSource bossSecondStageAudio;
     public AudioSource bossFinalStandAudio;
+    public AudioSource battleEndedAudio;
     public AudioSource bossFightDefeatStartAudio;
     public AudioSource bossFightDefeatLoopAudio;
-    public AudioSource battleEndedAudio;
-
-    public float bossFightHealthThreshold = 66f;
-    public float bossSecondStageHealthThreshold = 33f;
 
     private enum GameState
     {
+        StartGameplay,
         Gameplay,
         BossFight,
         BossSecondStage,
         BossFinalStand,
+        BattleEnded,
         BossFightDefeat
     }
 
-    private GameState currentState = GameState.Gameplay;
+    private GameState currentState;
 
     private void Start()
     {
+        currentState = GameState.StartGameplay;
+
         // Start playing the "Start Gameplay" audio
         startGameplayAudio.Play();
     }
@@ -39,74 +39,70 @@ public class AudioManager : MonoBehaviour
     {
         switch (currentState)
         {
-            case GameState.Gameplay:
-                // Check if "Start Gameplay" audio has finished playing
+            case GameState.StartGameplay:
                 if (!startGameplayAudio.isPlaying)
                 {
-                    // Start playing "Gameplay Loop" audio in a loop
-                    gameplayLoopAudio.Play();
+                    StartGameplayLoop();
                 }
+                break;
 
-                // Check if player is defeated
-                // Replace this with your own logic for player defeat condition
+            case GameState.Gameplay:
                 if (PlayerDefeated())
                 {
-                    // Transition to end gameplay state
                     TransitionToEndGameplay();
                 }
                 break;
 
             case GameState.BossFight:
-                // Check if player is defeated during boss fight
                 if (PlayerDefeated())
                 {
-                    // Transition to boss fight defeat state
                     TransitionToBossFightDefeat();
                 }
-                else if (BossHealthBelowThreshold(bossFightHealthThreshold))
+                else if (BossHealthBelowThreshold(66))
                 {
-                    // Transition to boss second stage
                     TransitionToBossSecondStage();
                 }
                 break;
 
             case GameState.BossSecondStage:
-                // Check if player is defeated during boss second stage
                 if (PlayerDefeated())
                 {
-                    // Transition to boss fight defeat state
                     TransitionToBossFightDefeat();
                 }
-                else if (BossHealthBelowThreshold(bossSecondStageHealthThreshold))
+                else if (BossHealthBelowThreshold(33))
                 {
-                    // Transition to boss final stand
                     TransitionToBossFinalStand();
                 }
                 break;
 
             case GameState.BossFinalStand:
-                // Check if player is defeated during boss final stand
                 if (PlayerDefeated())
                 {
-                    // Transition to boss fight defeat state
                     TransitionToBossFightDefeat();
                 }
-                else if (BossHealthBelowThreshold(0f))
+                else if (BossHealthBelowThreshold(0))
                 {
-                    // Transition to battle ended state
                     TransitionToBattleEnded();
                 }
                 break;
 
+            case GameState.BattleEnded:
+                // Handle any logic for the battle ended state
+                break;
+
             case GameState.BossFightDefeat:
-                // Check if "Boss Fight Defeat Start" audio has finished playing
-                if (!bossFightDefeatStartAudio.isPlaying)
-                {
-                    // Start playing "Boss Fight Defeat Loop" audio in a loop
-                    bossFightDefeatLoopAudio.Play();
-                }
+                // Handle any logic for the boss fight defeat state
                 break;
         }
+    }
+
+    private void StartGameplayLoop()
+    {
+        // Start playing "Gameplay Loop" audio in a loop
+        gameplayLoopAudio.loop = true;
+        gameplayLoopAudio.Play();
+
+        currentState = GameState.Gameplay;
     }
 
     private bool PlayerDefeated()
@@ -115,9 +111,9 @@ public class AudioManager : MonoBehaviour
         return false;
     }
 
-    private bool BossHealthBelowThreshold(float threshold)
+    private bool BossHealthBelowThreshold(int threshold)
     {
-        // Replace this with your own logic to check boss health
+        // Replace this with your own logic to check if the boss health is below the threshold
         return false;
     }
 
@@ -129,7 +125,7 @@ public class AudioManager : MonoBehaviour
         // Fade in "End Gameplay" audio
         FadeInAudio(endGameplayAudio);
 
-        currentState = GameState.Gameplay;
+        currentState = GameState.BattleEnded;
     }
 
     private void TransitionToBossFight()
@@ -173,18 +169,36 @@ public class AudioManager : MonoBehaviour
         // Fade in "The Battle Has Ended" audio
         FadeInAudio(battleEndedAudio);
 
-        currentState = GameState.Gameplay;
+        currentState = GameState.BattleEnded;
     }
 
     private void TransitionToBossFightDefeat()
     {
-        // Fade out currently playing audio
+        // Fade out the currently playing audio (either "Boss Fight", "Boss Second Stage", or "Boss Final Stand")
         FadeOutCurrentAudio();
 
-        // Start playing "Boss Fight Defeat Start" audio
-        bossFightDefeatStartAudio.Play();
+        // Fade in "Boss Fight Defeat Start" audio
+        FadeInAudio(bossFightDefeatStartAudio);
 
         currentState = GameState.BossFightDefeat;
+    }
+
+    private void FadeOutCurrentAudio()
+    {
+        switch (currentState)
+        {
+            case GameState.BossFight:
+                FadeOutAudio(bossFightAudio);
+                break;
+
+            case GameState.BossSecondStage:
+                FadeOutAudio(bossSecondStageAudio);
+                break;
+
+            case GameState.BossFinalStand:
+                FadeOutAudio(bossFinalStandAudio);
+                break;
+        }
     }
 
     private void FadeOutAudio(AudioSource audioSource)
@@ -197,33 +211,7 @@ public class AudioManager : MonoBehaviour
         StartCoroutine(FadeIn(audioSource, 1f));
     }
 
-    private void FadeOutCurrentAudio()
-    {
-        switch (currentState)
-        {
-            case GameState.Gameplay:
-                FadeOutAudio(gameplayLoopAudio);
-                break;
-
-            case GameState.BossFight:
-                FadeOutAudio(bossFightAudio);
-                break;
-
-            case GameState.BossSecondStage:
-                FadeOutAudio(bossSecondStageAudio);
-                break;
-
-            case GameState.BossFinalStand:
-                FadeOutAudio(bossFinalStandAudio);
-                break;
-
-            case GameState.BossFightDefeat:
-                FadeOutAudio(bossFightDefeatLoopAudio);
-                break;
-        }
-    }
-
-    private IEnumerator FadeOut(AudioSource audioSource, float fadeDuration)
+    private System.Collections.IEnumerator FadeOut(AudioSource audioSource, float fadeDuration)
     {
         float startVolume = audioSource.volume;
         float startTime = Time.time;
@@ -242,7 +230,7 @@ public class AudioManager : MonoBehaviour
         audioSource.volume = startVolume;
     }
 
-    private IEnumerator FadeIn(AudioSource audioSource, float fadeDuration)
+    private System.Collections.IEnumerator FadeIn(AudioSource audioSource, float fadeDuration)
     {
         audioSource.volume = 0;
         audioSource.Play();
@@ -263,3 +251,4 @@ public class AudioManager : MonoBehaviour
         audioSource.volume = targetVolume;
     }
 }
+

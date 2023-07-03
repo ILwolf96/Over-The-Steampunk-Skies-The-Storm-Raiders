112200cfd02b434276b161de40f6e071a91d7ce5
SaharGGJ <124356732+SaharGGJ@users.noreply.github.com>
2023-07-03 15:39:28 +0300

    Fixed shit

--- Assets/Scripts/Storm Scripts/StormsRotation.cs
+++ Assets/Scripts/Storm Scripts/StormsRotation.cs
@@ -2,35 +2,36 @@ using UnityEngine;
 
 public class StormsRotation : MonoBehaviour
 {
-    [SerializeField] private Transform objectToRotateAround;
-    [SerializeField] private float distanceFromObject = 1f;
+    [SerializeField] private GameObject objectToRotateAround;
+    //[SerializeField] private float distanceFromObject = 1f;
     [SerializeField] private float rotationSpeed = 100f;
 
-    [SerializeField] private SpriteRenderer[] sprites;
-    [SerializeField] private float[] rotationDegrees;
-    [SerializeField] private float startingRotation = 0f;
+    //[SerializeField] private SpriteRenderer[] sprites;
+    //[SerializeField] private float[] rotationDegrees;
+    //[SerializeField] private float startingRotation = 0f;
 
     private void Start()
     {
         // Initialize the starting rotation for each sprite
-        for (int i = 0; i < rotationDegrees.Length; i++)
-        {
-            rotationDegrees[i] += startingRotation;
-        }
+        //for (int i = 0; i < rotationDegrees.Length; i++)
+        //{
+        //    rotationDegrees[i] += startingRotation;
+        //}
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        for (int i = 0; i < sprites.Length; i++)
-        {
-            if (sprites[i] != null)
-            {
-                float angle = rotationDegrees[i] + Time.time * rotationSpeed;
-                Vector3 offset = Quaternion.Euler(0f, 0f, angle) * (Vector3.right * distanceFromObject);
-                sprites[i].transform.position = objectToRotateAround.position + offset;
+        objectToRotateAround.transform.Rotate(new Vector3(0,0,rotationSpeed/10));
+        //for (int i = 0; i < sprites.Length; i++)
+        //{
+        //    if (sprites[i] != null)
+        //    {
+        //        float angle = rotationDegrees[i] + Time.time * rotationSpeed;
+        //        Vector3 offset = Quaternion.Euler(0f, 0f, angle) * (Vector3.right * distanceFromObject);
+        //        sprites[i].transform.position = objectToRotateAround.position + offset;
 
-                sprites[i].transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
-            }
-        }
+        //        sprites[i].transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        //    }
+        //}
     }
 }
